532602cd027d45389e043a8d6de14b1404cd2640
Demetrius <wh46tesmoke@gmail.com>
2021-07-22 23:50:04 +0300

    ver0.1

---
+++ bot.js
@@ -0,0 +1,17 @@
+var token = '1827597556:AAGhYI2HQ3z1wqMwnEKeBfH-3hOfCCTFTQw';
+
+var Bot = require('node-telegram-bot-api'),
+    bot = new Bot(token, { polling: true });
+
+console.log('bot server started...');
+
+
+
+bot.on('message', (msg) => {
+  if (msg.new_chat_members != undefined) {
+    bot.sendMessage(msg.chat.id, 'Вітаємо вас на телеграм канали Вараш.people');
+  }
+});
+
+
+
